1ba8d67d00e8eb715c189acd37a7e3e810a33b28
Stanislav <s.propisnov@trendline.in.ua>
2018-03-10 15:51:31 +0200

    s

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
+import java.util.Scanner;
+
+
+
+
 public class Main {
 
+    public static int rnd(int min, int max)
+    {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        int num;
+        int maxnum;
+        int counts;
+
+        System.out.println("Введите крайнее число диопозона");
+        maxnum = in.nextInt();
+        System.out.println("Введите кол-во попыток");
+        counts = in.nextInt();
+
+        final int min = 1; // Минимальное число для диапазона
+        final int max = maxnum; // Максимальное число для диапазона
+        final int rnd = rnd(min, max);
+
+        int i = 0;
+        while (i < counts) {
+            System.out.println("Введите число");
+            num = in.nextInt();
+            if (num == rnd) {
+                System.out.println("Вы угадали");
+                break;
+            } else if (num > rnd) {
+                System.out.println("Ваше число больше");
+            } else if (num < rnd) {
+                System.out.println("Ваше число меньше");
+            }
+
+            i++;
+            if(i == counts){
+                System.out.println("Попытки закончились");
+            }
+
+        }
+
+
+        System.out.println("Загаданное число: "+rnd);
+
     }
 }
